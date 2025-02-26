9e7aab8a19448cb908bfb9d180147476249f9192
guilhe <grutenberg@dc.uba.ar>
2025-02-26 18:38:43 -0300

    feat(Report): unnecesary columns deletion

--- main.py
+++ main.py
@@ -6,15 +6,20 @@ class RentersInsuranceReport(object):
     def __init__(self, input_url: str) -> None:
         self.dataframe = pd.read_excel(input_url)
 
-        # Delete first 3 rows
-        self.dataframe.drop([0, 1, 3], inplace=True, axis=0)
+        # Delete first 2 rows
+        self.dataframe.drop([0, 1], inplace=True, axis=0)
+
+        # Merge Second with first row
+        self.dataframe.iloc[0] = self.dataframe.iloc[0].str.cat(
+            self.dataframe.iloc[1].fillna(''), sep=' ').str.strip()
 
         # Rename columns with current first row
         self.dataframe.columns = self.dataframe.iloc[0]
 
-        # Delete what was the row with the names of the columns
-        self.dataframe.drop([2], inplace=True, axis=0)
+        # Delete what were the rows with the names of the columns
+        self.dataframe.drop([2, 3], inplace=True, axis=0)
 
+        print(self.dataframe)
         # Renaming tenant id to property id
         last_property = None
         for index, row in self.dataframe.iterrows():
@@ -29,8 +34,25 @@ class RentersInsuranceReport(object):
         self.dataframe = self.dataframe.dropna(subset=['Unit'])
 
         # Delete unnecesary columns
-        self.dataframe.drop(
-            ['Occupancy', 'Policy', 'Rental Type', 'Insurance Company', 'Cancellation', 'Reinstate', 'Lease'], inplace=True, axis=1)
+        self.dataframe.drop([
+            'Occupancy Status',
+            'Policy Status',
+            'Rental Type',
+            'Insurance Company',
+            'Policy Number',
+            'Policy Title',
+            'Lease From',
+            'Lease To',
+            'Policy Begin',
+            'Cancellation Date',
+            'Reinstate Date',
+            'Master Policy',
+            'Pet Endorsement',
+            'Interested Party'
+        ], inplace=True, axis=1)
+        self.dataframe.drop(self.dataframe.columns[5], axis=1, inplace=True)
+
+        self.dataframe['Name'], self.dataframe['Unit'] = self.dataframe['Unit'], self.dataframe['Name']
 
         print("DATAFRAME")
         print(self.dataframe)
